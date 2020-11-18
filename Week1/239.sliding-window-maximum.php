aebce97b5e9c49088f5231cd48873b9247afa32b
wangyoukun <1171725650@qq.com>
2020-11-18 17:16:22 +0800

    滑动窗口最大值

---
+++ Week1/239.sliding-window-maximum.php
@@ -0,0 +1,44 @@
+<?php
+
+class Solution
+{
+    /**
+     * 一、双端单调队列 O(n) O(k)
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return Integer[]
+     */
+    function maxSlidingWindow($nums, $k)
+    {
+        $count = count($nums);
+        $deque = new SplQueue();
+        $ret = [];
+
+        for ($i = 0; $i < $k; $i++) {
+            while (!$deque->isEmpty() && $deque->top() < $nums[$i]) {
+                $deque->pop();
+            }
+            $deque->enqueue($nums[$i]);
+        }
+        $ret[] = $deque->bottom();
+        for ($i = $k; $i < $count; $i++) {
+            if ($nums[$i - $k] == $deque->bottom()) {
+                $deque->dequeue();
+            }
+            while (!$deque->isEmpty() && $deque->top() < $nums[$i]) {
+                $deque->pop();
+            }
+            $deque->enqueue($nums[$i]);
+            $ret[] = $deque->bottom();
+        }
+        return $ret;
+    }
+}
+
+$in = [1, 3, -1, -3, 5, 3, 6, 7];
+$in = [1, -1];
+$k = 3;
+$k = 1;
+$s = new Solution();
+$ret = $s->maxSlidingWindow($in, $k);
+print_r($ret);
