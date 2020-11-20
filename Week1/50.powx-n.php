c4bf0c67424fbc98387d04348bf2f0d4546bbf5f
wangyoukun <1171725650@qq.com>
2020-11-20 12:36:15 +0800

    求X的N次幂

---
+++ Week1/50.powx-n.php
@@ -0,0 +1,63 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * 一、递归快速幂 O(logN) O(logN)
+     * @param Float $x
+     * @param Integer $n
+     * @return Float
+     */
+    function myPow1($x, $n)
+    {
+        if ($n < 0) {
+            return 1 / $this->dg($x, -$n);
+        } else {
+            return $this->dg($x, $n);
+        }
+    }
+
+    function dg($x, $n)
+    {
+        if ($n == 0) return 1;
+        $half = $this->dg($x, $n / 2);
+        if ($n % 2 == 0) {
+            return $half * $half;
+        } else {
+            return $half * $half * $x;
+        }
+    }
+
+    /**
+     * 二、迭代快速幂 O(logN) O(1)
+     * @param Float $x
+     * @param Integer $n
+     * @return Float
+     */
+    function myPow($x, $n)
+    {
+        if ($n < 0) {
+            return 1 / $this->dd($x, -$n);
+        } else {
+            return $this->dd($x, $n);
+        }
+    }
+
+    function dd($x,$n) {
+        if ($n == 0) return 1;
+        $ret = 1;
+        while ($n) {
+            if (($n & 1)) $ret *= $x;
+            $x *= $x;
+            $n >>= 1;
+        }
+        return $ret;
+    }
+}
+
+$x = 2;
+$n = -2;
+$s = new Solution();
+$ret = $s->myPow($x, $n);
+print_r($ret);
