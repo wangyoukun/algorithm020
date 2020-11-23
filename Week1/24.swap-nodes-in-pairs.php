e26311a70ae7b349d71de5d99e1e09e87bd2d584
wangyoukun <1171725650@qq.com>
2020-11-23 11:20:33 +0800

    两两交换链表中节点增加 栈方式

--- Week1/24.swap-nodes-in-pairs.php
+++ Week1/24.swap-nodes-in-pairs.php
@@ -25,12 +25,12 @@ class Solution
      */
     function swapPairs1($head)
     {
-        $dummyHead = new ListNode(-1);
-        $dummyHead->next = $head;
-        $temp = $dummyHead;
+        $dummyHead = new ListNode(-1);//哨兵节点
+        $dummyHead->next = $head; //哨兵节点连线
+        $temp = $dummyHead; //遍历指针
         while ($temp->next && $temp->next->next) {
-            $si = $temp->next;
-            $fi = $temp->next->next;
+            $si = $temp->next; //慢指针 指向第一个节点
+            $fi = $temp->next->next; //快指针 指向第二个节点
             $temp->next = $fi;
             $si->next = $fi->next;
             $fi->next = $si;
@@ -40,11 +40,11 @@ class Solution
     }
 
     /**
-     * 二、递归 O(n) O(n)
+     * 一、递归 O(n) O(n)
      * @param ListNode $head
      * @return ListNode
      */
-    function swapPairs($head)
+    function swapPairs2($head)
     {
         if (!$head || !$head->next) {
             return $head;
@@ -54,4 +54,33 @@ class Solution
         $newHead->next = $head;
         return $newHead;
     }
+
+    /**
+     * 三、栈 O(n) O(1)
+     * @param ListNode $head
+     * @return ListNode
+     */
+    function swapPairs($head)
+    {
+        if (!$head || !$head->next) {
+            return $head;
+        }
+        $dummyHead = new ListNode(-1); //哨兵节点
+        $dummyHead->next = $head; //给哨兵节点连线
+        $p = $dummyHead; //慢指针 用来串节点
+        $cur = $head;  //快指针 用来遍历
+        $stack = new SplStack();
+        while ($cur && $cur->next) {
+            $stack->push($cur);
+            $stack->push($cur->next);
+            $cur = $cur->next->next;
+            $p->next = $stack->pop();
+            $p = $p->next;
+            $p->next = $stack->pop();
+            $p = $p->next;
+        }
+        $p->next = $cur ? $cur : null; //处理尾巴
+        return $dummyHead->next;
+    }
+
 }
