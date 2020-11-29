c7baf275f3c6ae27e9a31a0940de6cee5e58ccd5
wangyoukun <1171725650@qq.com>
2020-11-29 15:48:35 +0800

    滑动窗口最大值 增加动态规划解法

--- Week1/239.sliding-window-maximum.php
+++ Week1/239.sliding-window-maximum.php
@@ -3,7 +3,8 @@
 class Solution
 {
     /**
-     * 一、双端单调队列 O(n) O(k)
+     * 一、双端单调队列 O(N) O(N)
+     * 输出数组使用了O(N−k+1) 空间，双向队列使用了O(k)
      * @param Integer[] $nums
      * @param Integer $k
      * @return Integer[]
@@ -34,16 +35,49 @@ class Solution
         }
         return $ret;
     }
+
+    /**
+     * 二、动态规划 O(N) O(N)
+     * dp(i,j) = max(dp(i,x) = right[i],dp(x,j) = left[j])
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return Integer[]
+     */
+    function maxSlidingWindow2($nums, $k)
+    {
+        if ($k == 0 || empty($nums)) return [];
+        $count = count($nums);
+        $ret = [];
+        $left[0] = $nums[0];
+        $right[$count - 1] = $nums[$count - 1];
+        for ($i = 1; $i < $count; $i++) {
+            if ($i % $k == 0) {
+                $left[$i] = $nums[$i];
+            } else {
+                $left[$i] = max($nums[$i], $left[$i - 1]);
+            }
+            $j = $count - $i - 1;
+            if (($j + 1) % $k == 0) {
+                $right[$j] = $nums[$j];
+            } else {
+                $right[$j] = max($nums[$j], $right[$j + 1]);
+            }
+        }
+        for ($i = 0; $i < $count - $k + 1; $i++) {
+            $ret[] = max($right[$i], $left[$i + $k - 1]);
+        }
+        return $ret;
+    }
 }
 
 $in = [1, 3, -1, -3, 5, 3, 6, 7];
 $in = [1, -1];
 $in = [];
-$in = [1,3,1,2,0,5];
+$in = [1, 3, 1, 2, 0, 5];
 $k = 3;
 $k = 1;
 $k = 0;
 $k = 3;
 $s = new Solution();
-$ret = $s->maxSlidingWindow($in, $k);
+$ret = $s->maxSlidingWindow2($in, $k);
 print_r($ret);
