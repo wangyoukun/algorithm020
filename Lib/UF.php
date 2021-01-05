dd20077bb59ac097ff5a13f5671b309c1d9dc5d6
wangyoukun <1171725650@qq.com>
2021-01-05 17:40:05 +0800

    增加并查集

---
+++ Lib/UF.php
@@ -0,0 +1,46 @@
+<?php
+
+class UF
+{
+    function __construct($n)
+    {
+        $this->count = 0; //连通分量
+        $this->parent = []; // 森林
+        $this->size = []; // 树大小
+        for ($i = 0; $i < $n; $i++) {
+            $this->parent[$i] = $i;
+            $this->size[$i] = 1;
+            $this->count++;
+        }
+    }
+
+    function find($x)
+    {
+        while ($this->parent[$x] != $x) {
+            $this->parent[$x] = $this->parent[$this->parent[$x]];
+            $x = $this->parent[$x];
+        }
+        return $x;
+    }
+
+    function union($p, $q)
+    {
+        $rootP = $this->find($p);
+        $rootQ = $this->find($q);
+        if ($rootP == $rootQ) return;
+
+        if ($this->size[$rootP] > $this->size[$rootQ]) {
+            $this->parent[$rootQ] = $rootP;
+            $this->size[$rootP] += $this->size[$rootQ];
+        } else {
+            $this->parent[$rootP] = $rootQ;
+            $this->size[$rootQ] += $this->size[$rootP];
+        }
+        $this->count--;
+    }
+
+    function count()
+    {
+        return $this->count;
+    }
+}
